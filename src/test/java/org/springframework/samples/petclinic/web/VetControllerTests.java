fc9027dd20b9459684731aed3b178dc2d840f812
Abrcobgal <abrahamconil555@gmail.com>
2021-04-04 13:03:11 +0200

    Se han realizado dos cambios
    ·añadidas validaciones a fechas de nacimiento de mascotas y de
    realización de visitas
    
    ·eliminados warnings de imports no usados en el código

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
@@ -9,10 +9,7 @@ import org.springframework.samples.petclinic.configuration.SecurityConfiguration
 import org.springframework.samples.petclinic.model.Specialty;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.service.VetService;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.xml.HasXPath.hasXPath;
 import static org.mockito.BDDMockito.given;
