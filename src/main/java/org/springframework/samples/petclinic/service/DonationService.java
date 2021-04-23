00f06e0ad7de0b2b672a4f919a294d508fef43b9
mangarmar <mangarmar17@alum.us.es>
2021-04-23 16:06:44 +0200

    #56 Crear nuevas donaciones

--- src/main/java/org/springframework/samples/petclinic/service/DonationService.java
+++ src/main/java/org/springframework/samples/petclinic/service/DonationService.java
@@ -50,9 +50,40 @@ private CauseRepository causeRepository;
 
 	@Transactional(readOnly = true)
 	public Double totalDonationByCause(int causeId){
-		return findDonationsByCauseId(causeId).stream().mapToDouble(x->x.getAmount()).sum();
+		return findDonationsByCauseId(causeId).stream().mapToDouble(x->Double.valueOf(x.getAmount())).sum();
 		
 	}
+
+	public boolean validDonation(Donation donation, Cause cause) {
+		String amount = donation.getAmount();
+		Boolean empty = !amount.isEmpty();
+		Boolean isNumber = amount.matches("[0-9]*");
+		Boolean isZero = amount.matches("0");
+		Boolean max = Double.valueOf(donation.getAmount()) <= (cause.getTarget() - cause.getGathered());
+		
+		return empty && isNumber && !isZero && max;
+	}
+	
+	public boolean validNegativeDonation(Donation donation) {
+		String amount = donation.getAmount();
+		Boolean isNegative = amount.contains("-");
+
+		
+		return isNegative;
+	}
+	
+	public boolean validIsZero(Donation donation) {
+		String amount = donation.getAmount();
+		Boolean isZero = Double.valueOf(amount)==0;
+		return isZero;
+	}
+
+	public boolean validMaxDonation(Donation donation, Cause cause) {
+		
+		return Double.valueOf(donation.getAmount()) > (cause.getTarget() - cause.getGathered());
+	}
+	
+
 
 }
 
--- src/main/java/org/springframework/samples/petclinic/web/CauseController.java
+++ src/main/java/org/springframework/samples/petclinic/web/CauseController.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic.web;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
@@ -12,40 +13,101 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Cause;
 import org.springframework.samples.petclinic.model.Donation;
+import org.springframework.samples.petclinic.model.Hotel;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.CauseService;
 import org.springframework.samples.petclinic.service.DonationService;
+import org.springframework.samples.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CauseController {
 
 //	private static final String VIEWS_CAUSE_CREATE_OR_UPDATE_FORM = "causes/createOrUpdateCauseForm";
-	
+
 	private final CauseService causeService;
 	private final DonationService donationService;
-	
+	private final OwnerService ownerService;
+
 	@Autowired
-	public CauseController(CauseService causeService, DonationService donationService) {
+	public CauseController(CauseService causeService, DonationService donationService, OwnerService ownerService) {
 		this.causeService = causeService;
 		this.donationService = donationService;
+		this.ownerService = ownerService;
 	}
-	
-	@GetMapping(value = {"/causes"})
+
+	@GetMapping(value = { "/causes" })
 	public String showCauseList(Map<String, Object> model) {
 		Collection<Cause> causes = causeService.findUnfinishedCauses();
 		model.put("causes", causes);
 		return "causes/causeList";
 	}
-	
-	@GetMapping(value = {"/causes/{causeId}/details"})
+
+	@GetMapping(value = { "/causes/{causeId}/details" })
 	public String showDetailsCauseList(@PathVariable("causeId") int causeId, Map<String, Object> model) {
 		Cause cause = causeService.findCauseById(causeId);
-		List<Donation> donations =  donationService.findDonationsByCauseId(causeId).stream().collect(Collectors.toList());
+		List<Donation> donations = donationService.findDonationsByCauseId(causeId).stream()
+				.collect(Collectors.toList());
 		donations.sort(Comparator.comparing(Donation::getDate));
 		model.put("donations", donations);
 		model.put("cause", cause);
 		return "causes/causeDetailsList";
 	}
+
+	@GetMapping(value = { "/causes/{causeId}/donation" })
+	public String newDonation(@PathVariable("causeId") int causeId, Map<String, Object> model) {
+
+		Integer ownerId = ownerService.getOwnerId();
+		Owner owner = ownerService.findOwnerById(ownerId);
+		Cause cause = causeService.findCauseById(causeId);
+		Double max = cause.getTarget() - cause.getGathered();
+		Donation donation = new Donation();
+		LocalDate date = LocalDate.now();
+		model.put("max", max);
+		model.put("owner", owner);
+		model.put("cause", cause);
+		model.put("date", date.toString());
+		model.put("donation", donation);
+
+		return "causes/newDonation";
+	}
+
+	@PostMapping(value = { "/causes/{causeId}/donation" })
+	public String saveDonation(@PathVariable("causeId") int causeId, Donation donation, ModelMap model) {
+
+		Cause cause = causeService.findCauseById(causeId);
+		if (donationService.validDonation(donation, cause)) {
+			Integer ownerId = ownerService.getOwnerId();
+			Owner owner = ownerService.findOwnerById(ownerId);
+			donation.setOwner(owner);
+			donation.setCause(cause);
+			donationService.saveDonation(donation);
+			Double amount = Double.valueOf(donation.getAmount());
+			cause.setGathered(cause.getGathered() + amount);
+			causeService.saveCause(cause);
+			model.addAttribute("message", "Donación creada con éxito");
+			model.addAttribute("owner", owner);
+			model.addAttribute("cause", cause);
+			return showCauseList(model);
+
+		} else if(donationService.validIsZero(donation)){
+			model.put("message", "La donación no puede ser de 0 euros");
+			return newDonation(causeId, model);
+		}else if(donationService.validNegativeDonation(donation)) {
+			model.put("message", "La donación no puede ser negativa");
+			return newDonation(causeId, model);
+		}else if(donationService.validMaxDonation(donation, cause)){
+			model.put("message", "La donación no puede ser superior al máximo");
+			return newDonation(causeId, model);
+		}else {
+			model.put("message", "Porfavor, indique la cantidad con números enteros");
+			return newDonation(causeId, model);
+		}
+
+	}
+
 }
